7f38f942a5531aa972407dedbb9ff941272f7b52
KingaSzmyd <kingaszmyd@gmail.com>
2022-01-30 19:43:44 +0000

    Fixed bug with stripe payments

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -9,8 +9,6 @@
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var Stripe = [];
-var $ = [];
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
