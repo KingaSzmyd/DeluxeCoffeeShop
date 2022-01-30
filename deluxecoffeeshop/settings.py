ca39a3fc8973868a096a5b0092b5ae942acdea87
KingaSzmyd <kingaszmyd@gmail.com>
2022-01-30 16:15:35 +0000

    Chaned debug to false

--- deluxecoffeeshop/settings.py
+++ deluxecoffeeshop/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = os.environ.get('DEVELOPMENT', False)
 
 ALLOWED_HOSTS = ['msp4-deluxecoffeeshop.herokuapp.com', 'localhost']
 
